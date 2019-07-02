03035f85f7cb374c38b8b8271cdabc9d0b1c10f7
oburdasov <ovburdasov@gmail.com>
2019-07-02 18:29:44 +0300

    another animation

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ for (let index = 0; index < columnLength; index++) {
   field.push(new Array(rowLength).fill(null));
 }
 let event;
-let update = animationThree;
+let update = animationOne;
 
 function animationOne() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -30,7 +30,7 @@ function animationOne() {
       ctx.strokeStyle = `rgba(${shift * 100},106,173, ${shift / 5}`;
 
       ctx.beginPath();
-      ctx.arc(x + (500 / coefX * coefY), y + (500 / coefX * coefY), shift, 0, 2 * Math.PI);
+      ctx.arc(x + (500 / coefX * coefY), y + (500 / coefX * coefY), Math.min(shift, 50), 0, 2 * Math.PI);
       ctx.stroke();
     }
   }
@@ -73,24 +73,24 @@ function animationThree() {
   }
 }
 
-// function animationFour() {
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   for (let i = 0; i < rowLength; i++) {
-//     for (let j = 0; j < columnLength; j++) {
-//       let x = i * size;
-//       let y = j * size;
-//       let coefX = event.clientX - x;
-//       let coefY = event.clientY - y;
-//       let commonCoef = Math.abs(coefY) + Math.abs(coefX);
-//       let shift = 100 / Math.abs(commonCoef) + 0.5;
-//       ctx.strokeStyle = `rgba(${shift * 100},106,173, ${shift / 2}`;
+function animationFour() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < rowLength; i++) {
+    for (let j = 0; j < columnLength; j++) {
+      let x = i * size;
+      let y = j * size;
+      let coefX = event.clientX - x;
+      let coefY = event.clientY - y;
+      let commonCoef = Math.abs(coefY) + Math.abs(coefX);
+      let shift = 200 / Math.abs(commonCoef) + 0.1;
+      ctx.strokeStyle = `rgba(${shift * 20},${commonCoef / 5},${commonCoef / 5}, ${shift / 2}`;
 
-//       ctx.beginPath();
-//       ctx.arc(x + (100 / commonCoef * coefX), y + (100 / commonCoef * coefY), Math.min(500 / commonCoef, 10), 0, 2 * Math.PI);
-//       ctx.stroke();
-//     }
-//   }
-// }
+      ctx.beginPath();
+      ctx.arc(x + (1000 / commonCoef * coefX), y + (500 / commonCoef * coefY), Math.min(500 / commonCoef, 10), 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+  }
+}
 
 document.addEventListener('keydown', ev => {
   if (ev.key === '1') {
@@ -99,6 +99,8 @@ document.addEventListener('keydown', ev => {
     update = animationTwo;
   } else if (ev.key === '3') {
     update = animationThree;
+  } else if (ev.key === '4') {
+    update = animationFour;
   }
   update();
 })
