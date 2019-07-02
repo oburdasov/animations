f0347efbe3de457e296a1c66aa1de053a55c174f
oburdasov <ovburdasov@gmail.com>
2019-07-02 17:08:32 +0300

    third experiment + animations switch

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
 document.body.style.margin = 0;
 document.body.style.background = 'black';
+document.body.style.overflow = 'hidden'
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight - 4;
+canvas.height = window.innerHeight;
 let ctx = canvas.getContext('2d');
 
 let field = []
@@ -14,19 +15,20 @@ let columnLength = Math.floor(window.innerHeight / size);
 for (let index = 0; index < columnLength; index++) {
   field.push(new Array(rowLength).fill(null));
 }
+let event;
+let update = animationOne;
 
-function update(ev) {
+function animationOne() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (let i = 0; i < rowLength; i++) {
     for (let j = 0; j < columnLength; j++) {
       let x = i * size;
       let y = j * size;
-      let coefX = x - ev.clientX;
-      let coefY = y - ev.clientY;
+      let coefX = x - event.clientX;
+      let coefY = y - event.clientY;
       let shift = 400 / Math.abs(coefX)
       ctx.strokeStyle = `rgba(${shift * 100},106,173, ${shift / 5}`;
 
-      
       ctx.beginPath();
       ctx.arc(x + (500 / coefX * coefY), y + (500 / coefX * coefY), shift, 0, 2 * Math.PI);
       ctx.stroke();
@@ -34,21 +36,34 @@ function update(ev) {
   }
 }
 
-// function update(ev) {
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   for (let i = 0; i < rowLength; i++) {
-//     for (let j = 0; j < columnLength; j++) {
-//       let x = i * size;
-//       let y = j * size;
-//       let coefX = x / ev.clientX;
-//       let coefY = y / ev.clientY;
-//       ctx.strokeStyle = `rgba(140,106,173, ${(coefX * coefY)}`;
-      
-//       ctx.beginPath();
-//       ctx.arc(x + ((ev.clientX - x) * coefX), y + ((ev.clientX - x) * coefY), coefX * coefY * 10, 0, 2 * Math.PI);
-//       ctx.stroke();
-//     }
-//   }
-// }
+function animationTwo() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < rowLength; i++) {
+    for (let j = 0; j < columnLength; j++) {
+      let x = i * size;
+      let y = j * size;
+      let coefX = x - event.clientX;
+      let coefY = y - event.clientY;
+      let shift = 400 / Math.abs(coefX)
+      ctx.strokeStyle = `rgba(${shift * 100},106,173, ${shift / 5}`;
 
-document.addEventListener('mousemove', ev => update(ev))
+      ctx.beginPath();
+      ctx.arc(x + (500 / Math.sqrt(coefX)), y + (500 / coefX * coefY), Math.min(shift, 50), 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+  }
+}
+
+document.addEventListener('keydown', ev => {
+  if (ev.key === '1') {
+    update = animationOne;
+  } else if (ev.key === '2') {
+    update = animationTwo;
+  }
+})
+
+setInterval(() => {
+  update();
+}, 17)
+
+document.addEventListener('mousemove', ev => event = ev)
